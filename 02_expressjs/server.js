2533276a4b2e7092dfd82de96c850fa869a92749
ranselorm <ranselorm@outlook.com>
2026-01-16 07:20:08 +0000

    Refactor car API routes to use a dedicated router

--- 02_expressjs/server.js
+++ 02_expressjs/server.js
@@ -1,31 +1,30 @@
 import express from "express";
 
 const app = express();
-
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from Cars API");
-});
+const router = express.Router();
 
-app.get("/api/v1/cars", (req, res) => {
+router.get("/", (req, res) => {
   res.send("All cars endpoint");
 });
 
-app.post("/api/v1/cars", (req, res) => {
+router.post("/", (req, res) => {
   res.send("New car created");
 });
 
-app.put("/api/v1/cars/:id", (req, res) => {
+router.put("/:id", (req, res) => {
   res.send("Update specific car by id");
 });
 
-app.delete("/api/v1/cars/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
   res.send("Delete car");
 });
 
-app.get("/api/v1/cars/:id", (req, res) => {
+router.get("/:id", (req, res) => {
   res.send("Get one car by id");
 });
+
+app.use("/api/v1/cars", router);
 
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
